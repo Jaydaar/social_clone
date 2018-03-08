53b499f5c2901186fcc69baccc348a739ce5b3f1
Jaydaar <jaydaar@gmail.com>
2018-03-08 16:33:45 -0400

    Modified the templates and the views file in the groups app.

--- mysocial/groups/views.py
+++ mysocial/groups/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group 
+
+class SingleGroup(generic.DetailView):
+    models = Group 
+
+class ListGroups(generic.ListView):
+    model = Group
 # Create your views here.
