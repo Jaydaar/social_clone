969ccb15002769cf8031b0da88fb5ada7465f4d6
Jaydaar <jaydaar@gmail.com>
2018-03-11 09:13:26 -0400

    did some bug fixing.

--- mysocial/mysocial/urls.py
+++ mysocial/mysocial/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(), name='home'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('posts/', include('posts.urls', namespace='posts')),
+    path('groups/', include('groups.urls', namespace='groups')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('test/', views.TestPage.as_view(), name='test'),
     path('thanks/', views.ThanksPage.as_view(), name='thanks'),
--- mysocial/posts/views.py
+++ mysocial/posts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 
 from django.http import Http404
 from django.views import generic
